d5bd05a896749905a878f88fa9484fca1b95c19f
Sibi P <psibi2000@gmail.com>
2011-12-08 22:45:29 +0530

    Training Data Algorithm Selection added on gui interface

--- neuron.py
+++ neuron.py
@@ -10,6 +10,12 @@ class neuron:
         builder=gtk.Builder()
         builder.add_from_file(gladefile)
         self.window=builder.get_object("mainwindow")
+        self.algo_inc=builder.get_object("incremental_menuitem")
+        self.algo_batch=builder.get_object("batch_menuitem")
+        self.algo_rprop=builder.get_object("rprop_menuitem")
+        self.algo_qprop=builder.get_object("qprop_menuitem")
+        self.algorithm=False
+        self.algorithm_name=""
         builder.connect_signals(self)
 
     def on_mainwindow_destroy(self,widget,data=None):
@@ -17,7 +23,22 @@ class neuron:
 
     def on__Quit_activate(self,widget,data=None):
         gtk.main_quit()
-
+        
+    def on_algorithm_changed(self,radiomenuitem,data=None):
+        """This function is called whenever the radiomenuitem state is changed. This sets the algorithm of the training network"""
+        if self.algo_inc.get_active():
+            self.algorithm_name="FANN_TRAIN_INCREMENTAL"
+            self.algorithm=True
+        elif self.algo_batch.get_active():
+            self.algorithm_name="FANN_TRAIN_BATCH"
+            self.algorithm=True
+        elif self.algo_rprop.get_active():
+            self.algorithm_name="FANN_TRAIN_RPROP"
+            self.algorithm=True
+        elif self.algo_qprop.get_active():
+            self.algorithm_name="FANN_TRAIN_QUICKPROP"
+            self.algorithm=False
+            
 if __name__=="__main__":
     neuron_window=neuron()
     gtk.main()
