2cdffd8cdfebb4c3598fde205b9823bd759754e4
Sibi P <psibi2000@gmail.com>
2011-12-11 21:17:27 +0530

    Validation features added

--- train_window.py
+++ train_window.py
@@ -9,10 +9,53 @@ class train_window:
         gladefile="training_window.xml"
         builder=gtk.Builder()
         builder.add_from_file(gladefile)
+        self.connrate=builder.get_object("connrate_text")
+        self.lrate=builder.get_object("lrate_text")
+        self.derror=builder.get_object("derror_text")
+        self.miter=builder.get_object("miter_text")
+        self.ireport=builder.get_object("ireport_text")
+        self.function=builder.get_object("function_cb")
+        self.edialog=builder.get_object("errordialog")
         builder.connect_signals(self)
+
+    def on_train(self,widget,data=None):
+        if not self.Validate_form():
+            ed=errordialog()
+            ed.show()
+
+    def Validate_form(self):
+        if self.connrate.get_text_length()==0:
+            return False
+        elif self.lrate.get_text_length()==0:
+            return False
+        elif self.derror.get_text_length()==0:
+            return False
+        elif self.miter.get_text_length()==0:
+            return False
+        elif self.ireport.get_text_length()==0:
+            return False
+        elif self.function.get_active_text()==None:
+            return False
+        else:
+            return True
+    
+    def pack_param(self):
+        if self.Validate_form():
+            crate=self.connrate.get_text()
+            lrate=self.lrate.get_text()
+            derror=self.derror.get_text()
+            miter=self.miter.get_text()
+            ireport=self.ireport.get_text()
+            ofun=self.function.get_active_text()
+            tdata=[crate,lrate,derror,miter,ireport,ofun,tdata]
+            return tdata
+        else:
+            return None
+
 
     def on_train_window_destroy(self,widget,data=None):
         gtk.main_quit()
+
         
 if __name__=="__main__":
     create_window=train_window()
