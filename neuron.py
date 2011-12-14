89e6fb3c0ae11364892f2c12fe36789b2d7945ff
Sibi P <psibi2000@gmail.com>
2011-12-14 15:54:00 +0530

    File chooser dialog's events handled

--- neuron.py
+++ neuron.py
@@ -3,6 +3,7 @@ import sys
 import gtk
 import pygtk
 import dbm
+import glib
 from select_network import selectbpn_window
 
 class neuron:
@@ -18,10 +19,16 @@ class neuron:
         self.algo_rprop=builder.get_object("rprop_menuitem")
         self.algo_qprop=builder.get_object("qprop_menuitem")
         self.aboutdialog=builder.get_object("aboutdialog")
+        self.ntextview=builder.get_object("neuron_textview")
         self.algorithm=False
         self.algorithm_name=""
         builder.connect_signals(self)
-        
+
+    def write_neuron(self,string):
+        buffer=gtk.TextBuffer()
+        buffer.set_text(string)
+        self.ntextview.set_buffer(buffer)
+            
     def on_mainwindow_destroy(self,widget,data=None):
         gtk.main_quit()
 
@@ -58,6 +65,29 @@ class neuron:
     def get_training_data(self,widget,data=None):
         self.fcdialog.show()
 
+    def on_fc_cancel(self,widget,data=None):
+        self.fcdialog.hide()
+
+    def on_fc_ok(self,widget,data=None):
+        filename=self.fcdialog.get_filename()
+        self.fcdialog.hide()
+
+    def on_start_training(self,widget,data=None):
+        db=dbm.open('config.dat','c')
+        db['connrate']=self.connrate.get_text()
+        db['lrate']=self.lrate.get_text()
+        db['derror']=self.derror.get_text()
+        db['miter']=self.miter.get_text()
+        db['ireport']=self.ireport.get_text()
+        db['function']=self.function.get_active_text()
+        db['network']=self.network[0]
+        db['numl']=self.numl_text.get_text()
+        db['inputn']=self.inputn_text.get_text()
+        db['outputn']=self.outputn_text.get_text()
+        db['hiddenn']=self.hiddenn_text.get_text()
+        db['numh']=self.numh_text.get_text()
+
 if __name__=="__main__":
     neuron_window=neuron()
     gtk.main()
+
