b9f4a63ce5ea78ac46c8328c5fc2487f6e6582d8
Sibi P <psibi2000@gmail.com>
2011-12-15 11:00:14 +0530

    fann adjusted to take parameters from Neuron Dataset

--- simple_train.py
+++ simple_train.py
@@ -16,14 +16,30 @@ import sys
 #num_neurons_hidden=db['hiddenn']
 #numh_layers=db['numh']
 #db.close()
-connection_rate = 1
-learning_rate = 0.7
-num_input = 2
-num_neurons_hidden = 4
-num_output = 1
-desired_error = 0.0001
-max_iterations = 100000
-iterations_between_reports = 1000
+db=dbm.open('config.dat','c')
+talgo=db['Training Algorithm']
+bpn_type=db['Network Type']
+numl=int(db['Number of Layers'])
+num_input=int(db['Input Neurons'])
+num_output=int(db['Output Neurons'])
+num_neurons_hidden=int(db['Hidden Neurons'])
+num_hlay=int(db['Number of Hidden Layers'])
+connection_rate=float(db['Connection Rate'])
+learning_rate=float(db['Learning Rate'])
+desired_error=float(db['Desired Error'])
+max_iterations=int(db['Maximum Iterations'])
+iterations_between_reports=int(db['Iteration Between Reports'])
+ol_act_fun=db['Output Layer Activation Function']
+tfile=db['Training File']
+db.close()
+#connection_rate = 1
+#learning_rate = 0.7
+#num_input = 2
+#num_neurons_hidden = 4
+#num_output = 1
+#desired_error = 0.0001
+#max_iterations = 100000
+#iterations_between_reports = 1000
 
 class bpn:
 
@@ -41,7 +57,7 @@ class bpn:
         self.ann.create_sparse_array(connection_rate, (num_input, num_neurons_hidden, num_output))
         self.ann.set_learning_rate(learning_rate)
         self.ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
-        self.ann.train_on_file("xor.data", max_iterations, iterations_between_reports, desired_error)
+        self.ann.train_on_file(tfile, max_iterations, iterations_between_reports, desired_error)
         self.ann.save("xor_float.net")
 
     def print_param(self):
@@ -50,4 +66,5 @@ class bpn:
 
 if __name__=="__main__":
     network=bpn()
+
 
