f8745a8693499592de734e9a2cb55a948a83dc9d
Sibi P <psibi2000@gmail.com>
2011-12-20 22:37:22 +0530

    test.py Modularized

--- test.py
+++ test.py
@@ -1,44 +1,63 @@
 #!/usr/bin/python
 from pyfann import libfann
+import dbm
+import sys
 
-connection_rate = 1
-learning_rate = 0.7
-num_input = 2
-num_neurons_hidden = 4
-num_output = 1
+db=dbm.open('config.dat','c')
+talgo=db['Training Algorithm']
+bpn_type=db['Network Type']
+numl=int(db['Number of Layers'])
+num_input=int(db['Input Neurons'])
+num_output=int(db['Output Neurons'])
+num_neurons_hidden=int(db['Hidden Neurons'])
+num_hlay=int(db['Number of Hidden Layers'])
+connection_rate=float(db['Connection Rate'])
+learning_rate=float(db['Learning Rate'])
+desired_error=float(db['Desired Error'])
+max_iterations=int(db['Maximum Iterations'])
+iterations_between_reports=int(db['Iteration Between Reports'])
+ol_act_fun=db['Output Layer Activation Function']
+tfile=db['Training File']
+afile=db['Argument Data']
+db.close()
 
-desired_error = 0.0001
-max_iterations = 100000
-iterations_between_reports = 1000
+class function_aprox:
 
-ann = libfann.neural_net()
-ann.create_sparse_array(connection_rate, (num_input, num_neurons_hidden, num_output))
-ann.set_learning_rate(learning_rate)
-ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
+    def __init__(self):
+        self.ann = libfann.neural_net()
+        
+    def test(self):
+        self.ann.create_sparse_array(connection_rate, (num_input, num_neurons_hidden, num_output))
+        self.ann.set_learning_rate(learning_rate)
+        self.ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
+        self.ann.train_on_file(tfile, max_iterations, iterations_between_reports, desired_error)
+        print "\nFunction Approximation Details:"
+        fin=open("./dataset/xor.fa","r")
+        print ""
+        inputs=fin.readlines()
+        fin.close()
+        width=20
+        once=True
+        for ite in range(len(inputs)):
+            a=inputs.pop()
+            b=a.split(' ')
+            sample=b[1]
+            second=sample[:-1]
+            inpu=(int(b[0]),int(second))
+            calc_out=self.ann.run(inpu)
+            i=""
+            header=""
+            for x in range(len(inpu)):
+                i=i+"\t"+str(inpu[x]).ljust(width)
+                header=header+"\t"+"Input #"+str(x)
+                header.rjust(width)
+            if once:
+                print header+" ","Approximated Value"
+                once=False
+            print i+"\t",calc_out
 
-ann.train_on_file("../../examples/xor.data", max_iterations, iterations_between_reports, desired_error)
+if __name__=="__main__":
+    network=function_aprox()
+    network.test()
 
-fin=open("xor.fa","r")
-inputs=fin.readlines()
-fin.close()
-width=10
-once=True
-#inp1=((1,-1),(-1,1))
-for inp in inputs:
-    a=inputs.pop()
-    b=a.split(' ')
-    sample=b[1]
-    second=sample[:-1]
-    #print b
-    inpu=(int(b[0]),int(second))
-    calc_out=ann.run(inpu)
-    i=""
-    header=""
-    for x in range(len(inpu)):
-        i=i+" "+str(inpu[x]).ljust(9)
-        header=header+" "+"Input #"+str(x)
-        header.rjust(width)
-    if once:
-        print header+" ","Approximated Value"
-        once=False
-    print i+" ",calc_out
+
