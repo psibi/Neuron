e630f5575cda488a8e6a3da30a33af81f5a5383a
Sibi <psibi2000@gmail.com>
2011-12-31 04:09:52 +0530

    Neuron file changed. All warning removed. Components more modularized

--- neuron.py
+++ neuron.py
@@ -9,6 +9,7 @@ import shlex
 import shutil
 import subprocess
 from select_network import selectbpn_window
+from adv_train import adv_train
 
 class Neuron_TextViewOutput:
     def __init__(self,textview):
@@ -35,10 +36,6 @@ class neuron:
         self.aboutdialog=builder.get_object("aboutdialog")
         self.ntextview=builder.get_object("neuron_textview")
         self.talgo_window=builder.get_object("talgo_window")
-        self.vbox4=builder.get_object("vbox4")
-        self.vbox5=builder.get_object("vbox5")
-        self.adv_trainwindow=builder.get_object("adv_train_window")
-        self.af_liststore=builder.get_object("af_liststore")
         self.algorithm=False
         self.algorithm_name=""
         self.filename=""
@@ -173,23 +170,9 @@ class neuron:
         if output[0]:
             self.write_neuron(output[0])
 
-    def on_add_afn(self,button,data=None):
-        entry=gtk.Entry()
-        cb = gtk.ComboBox(self.af_liststore)
-        #self.entrys.append(entry)
-        self.vbox5.pack_start(cb, True, True, 1)
-        self.vbox4.pack_start(entry,True,True,1)
-        cb.show()
-        entry.show()
-
-    def on_adv_train_window_destroy(self,widget,data=None):
-        self.adv_trainwindow.hide()
-
-    def on_add_afl(self,button,data=None):
-        pass
-
     def on_train_config_activate(self,widget,data=None):
-        self.adv_trainwindow.show()
+        atrain=adv_train()
+        atrain.show()
 
 if __name__=="__main__":
     neuron_window=neuron()
