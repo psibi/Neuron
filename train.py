9967da4ee06dc3ad1adc9507cf01eb33133620ad
Sibi <psibi2000@gmail.com>
2012-01-12 01:52:50 +0530

    Added more parameters related to the simulator

--- train.py
+++ train.py
@@ -15,12 +15,6 @@ try:
     num_output=int(db['Output Neurons'])
     num_neurons_hidden=int(db['Hidden Neurons'])
     num_hlay=int(db['Number of Hidden Layers'])
-    connection_rate=float(db['Connection Rate'])
-    learning_rate=float(db['Learning Rate'])
-    desired_error=float(db['Desired Error'])
-    max_iterations=int(db['Maximum Iterations'])
-    iterations_between_reports=int(db['Iteration Between Reports'])
-    ol_act_fun=db['Output Layer Activation Function']
     tfile=db['Training File']
 except KeyError as key:
     dlg=gtk.MessageDialog(None,gtk.DIALOG_DESTROY_WITH_PARENT,gtk.MESSAGE_ERROR,gtk.BUTTONS_OK, str(key)+ " Uninitialized")
@@ -32,13 +26,20 @@ finally:
     db.close()
 
 class bpn:
-
     def __init__(self):
         print "Training Initialization taking place\n"
         self.ann = libfann.neural_net()
         self.network_file=""
 
     def train(self):
+        db=dbm.open('config.dat','c')
+        connection_rate=float(db['Connection Rate'])
+        learning_rate=float(db['Learning Rate'])
+        desired_error=float(db['Desired Error'])
+        max_iterations=int(db['Maximum Iterations'])
+        iterations_between_reports=int(db['Iteration Between Reports'])
+        ol_act_fun=db['Output Layer Activation Function']
+        db.close()
         if bpn_type=="SPR":
             self.ann.create_sparse_array(connection_rate, (num_input, num_neurons_hidden, num_output))
         elif bpn_type=="STD":
@@ -83,13 +84,72 @@ class bpn:
             self.ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
         elif ol_act_fun=="LINEAR FUNCTION":
             self.ann.set_activation_function_output(libfann.LINEAR)
+        #For Advanced Parameters related to Fixed Topology
+        try:
+            db=dbm.open('config.dat','c')
+            lmomentum=float(db['Learning Momentum'])
+            af_neuron_number=db['AF for Neuron']
+            af_n=db['AF Neuron']
+            af_layer_number=int(db['AF for layer'])
+            af_l=db['AF Layer']
+            asn=db['Activation Steepness for Neuron']
+            asl=db['Activation Steepness for layer']
+            tef=db['Train Error Function']
+            tsf=db['Train Stop Function']
+            bfl=float(db['Bit Fail Limit'])
+            db.close()
+        except KeyError:
+            pass
+        else:
+            self.ann.set_learning_momentum(lmomentum)
+            temp_list=af_neuron_number.split(",")
+            layer_no=int(temp_list[0])
+            neuron_no=int(temp_list[1])
+            steepness_list=asn.split(",")
+            svalue=float(steepness_list[0])
+            layer=int(steepness_list[1])
+            neuron=int(steepness_list[2])
+            steep_layer_list=asl.split(",")
+            vsteep=float(steep_layer_list[0])
+            vslayer=int(steep_layer_list[1])
+            if af_n=="LINEAR":
+                self.ann.set_activation_function(libfann.LINEAR,layer_no,neuron_no)
+            if af_l=="LINEAR":
+                self.ann.set_activation_function_layer(libfann.LINEAR,af_layer_number)
+            self.ann.set_activation_steepness(svalue,layer,neuron)
+            self.ann.set_activation_steepness_layer(vsteep,vslayer)
+            if tef=="LINEAR":
+                self.ann.set_train_error_function(libfann.ERRORFUNC_LINEAR)
+            if tsf=="MSE":
+                self.ann.set_train_stop_function(libfann.STOPFUNC_MSE)
+            self.ann.set_bit_fail_limit(bfl)
+        finally:
+            db.close()
         #Find Out Whether it is Evolving topology or Fixed Topology
         try:
             db=dbm.open('config.dat','c')
+            max_neurons=db['Maximum Neurons']
+            ncascade=True
+            db.close()
+        except KeyError:
+            ncascade=False
+        finally:
+            db.close()
+        if ncascade:
+            db=dbm.open('config.dat','c')
+            max_neurons=int(db['Maximum Neurons'])
+            neurons_between_reports=int(db['Neurons Between Reports'])
+            cdesired_error=float(db['Desired Error'])
+            db.close()
+        #For Advanced Cascade Parameters
+        try:
+            db=dbm.open('config.dat','c')
             ocf=db['Output Change Fraction']
+            db.close()
             tcascade=True
         except KeyError:
             tcascade=False
+
         if tcascade:
             db=dbm.open('config.dat','c')
             ocf=float(db['Output Change Fraction'])
@@ -113,7 +173,8 @@ class bpn:
             self.ann.set_cascade_min_out_epochs(min_oe)
             self.ann.set_cascade_max_cand_epochs(max_ce)
             self.ann.set_cascade_min_cand_epochs(min_ce)
-            self.ann.cascadetrain_on_data(tfile,max_neurons,neurons_between_reports,desired_error)
+        if ncascade:
+            self.ann.cascadetrain_on_file(tfile,max_neurons,neurons_between_reports,cdesired_error)
         else:
             self.ann.train_on_file(tfile, max_iterations, iterations_between_reports, desired_error)
         fileparts=tfile.split('/')
